0443f24e3b4447bdbdd42aff4462ee321fefeb6f
David M. Colvin <davidmcolvin@gmail.com>
2023-08-02 18:15:30 -0400

    Part of the form done.

--- TournamentTrackerAgain/TrackerUI/TournamentViewer.Designer.cs
+++ TournamentTrackerAgain/TrackerUI/TournamentViewer.Designer.cs
@@ -29,7 +29,65 @@
     private void InitializeComponent()
     {
       System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TournamentViewer));
+      this.headerLabel = new System.Windows.Forms.Label();
+      this.tournamentName = new System.Windows.Forms.Label();
+      this.roundLabel = new System.Windows.Forms.Label();
+      this.roundDropDown = new System.Windows.Forms.ComboBox();
+      this.checkBox1 = new System.Windows.Forms.CheckBox();
       this.SuspendLayout();
+      // 
+      // headerLabel
+      // 
+      this.headerLabel.AutoSize = true;
+      this.headerLabel.Font = new System.Drawing.Font("Segoe UI Light", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.headerLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+      this.headerLabel.Location = new System.Drawing.Point(12, 9);
+      this.headerLabel.Name = "headerLabel";
+      this.headerLabel.Size = new System.Drawing.Size(214, 50);
+      this.headerLabel.TabIndex = 0;
+      this.headerLabel.Text = "Tournament:";
+      // 
+      // tournamentName
+      // 
+      this.tournamentName.AutoSize = true;
+      this.tournamentName.Font = new System.Drawing.Font("Segoe UI Light", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.tournamentName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+      this.tournamentName.Location = new System.Drawing.Point(232, 9);
+      this.tournamentName.Name = "tournamentName";
+      this.tournamentName.Size = new System.Drawing.Size(150, 50);
+      this.tournamentName.TabIndex = 1;
+      this.tournamentName.Text = "<none>";
+      // 
+      // roundLabel
+      // 
+      this.roundLabel.AutoSize = true;
+      this.roundLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.roundLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+      this.roundLabel.Location = new System.Drawing.Point(21, 99);
+      this.roundLabel.Name = "roundLabel";
+      this.roundLabel.Size = new System.Drawing.Size(94, 37);
+      this.roundLabel.TabIndex = 2;
+      this.roundLabel.Text = "Round";
+      // 
+      // roundDropDown
+      // 
+      this.roundDropDown.FormattingEnabled = true;
+      this.roundDropDown.Location = new System.Drawing.Point(121, 98);
+      this.roundDropDown.Name = "roundDropDown";
+      this.roundDropDown.Size = new System.Drawing.Size(192, 38);
+      this.roundDropDown.TabIndex = 3;
+      // 
+      // checkBox1
+      // 
+      this.checkBox1.AutoSize = true;
+      this.checkBox1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.checkBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+      this.checkBox1.Location = new System.Drawing.Point(121, 172);
+      this.checkBox1.Name = "checkBox1";
+      this.checkBox1.Size = new System.Drawing.Size(160, 41);
+      this.checkBox1.TabIndex = 4;
+      this.checkBox1.Text = "checkBox1";
+      this.checkBox1.UseVisualStyleBackColor = true;
       // 
       // TournamentViewer
       // 
@@ -37,16 +95,28 @@
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.BackColor = System.Drawing.Color.White;
       this.ClientSize = new System.Drawing.Size(834, 491);
+      this.Controls.Add(this.checkBox1);
+      this.Controls.Add(this.roundDropDown);
+      this.Controls.Add(this.roundLabel);
+      this.Controls.Add(this.tournamentName);
+      this.Controls.Add(this.headerLabel);
       this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
       this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
       this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
       this.Name = "TournamentViewer";
       this.Text = "Tournament Viewer";
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
+
+    private System.Windows.Forms.Label headerLabel;
+    private System.Windows.Forms.Label tournamentName;
+    private System.Windows.Forms.Label roundLabel;
+    private System.Windows.Forms.ComboBox roundDropDown;
+    private System.Windows.Forms.CheckBox checkBox1;
   }
 }
 
